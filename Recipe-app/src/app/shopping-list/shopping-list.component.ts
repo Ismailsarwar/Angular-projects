7499f7c1127481bf86a8284bd110f7a2374131ca
Ismailsarwar <ismailsarwar18@gmail.com>
2024-02-14 16:47:50 +0500

    add new recipe app 2nd project

---
+++ Recipe-app/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrl: './shopping-list.component.css'
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients = [];
+
+  constructor() {}
+
+  ngOnInit() {
+    
+  }
+
+}
